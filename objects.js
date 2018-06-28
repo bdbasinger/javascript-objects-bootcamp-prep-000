672ae9d97b61aa5923361408c7113f5e942839bb
Brennan B <bdbasinger777@gmail.com>
2018-06-28 17:19:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,20 @@
 var playlist = { Maroon5: 'Girls like you' }
 
+function updatePlaylist(obj, key, value) {
+  return Object.assign( obj, { [key]: value } )
+}
+
+updatePlaylist(playlist, "Toby", "Feel it" )
+console.log(playlist) //
+
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+var ids = { Brennan: 1, James: 2, David: 3 }
+
+destructivelyUpdateObjectWithKeyAndValue(ids, "Ruben", 4)
+
+console.log(ids) // { Brennan: 1, James: 2, David: 3, Ruben: 4 }
